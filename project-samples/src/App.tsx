85dc1f7d0583de80c9065dc2596bb6dd66b077f0
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-06-06 16:39:42 +0330

    Updated

--- project-samples/src/App.tsx
+++ project-samples/src/App.tsx
@@ -1,25 +1,32 @@
 import "./App.css";
-import RenderMovies from "./components/render-movies";
-import Movie1Picture from "../public/assets/img/band-of-brothers.jpg";
-import Movie2Picture from "../public/assets/img/breaking-bad.jpeg";
-import Movie3Picture from "../public/assets/img/band-of-brothers.jpg";
-import Movie4Picture from "../public/assets/img/chernobyl.jpeg";
-import Movie5Picture from "../public/assets/img/sopranos.jpg";
-import Movie6Picture from "../public/assets/img/dark.jpg";
-import { MovieProps } from "./components/render-movies/components/movie";
+import CustomInput from "./components/custom-input";
 
 function App() {
-    const movies : MovieProps[] = [
-        { id: 1, rating: 4.5, title: "Hannibal", picture: Movie1Picture },
-        { id: 2, rating: 4.2, title: "Breaking Bad", picture: Movie2Picture },
-        { id: 3, rating: 3.7, title: "Band Of Brothers", picture: Movie3Picture },
-        { id: 4, rating: 4.9, title: "Chernobyl", picture: Movie4Picture },
-        { id: 5, rating: 4.4, title: "The Sopranos", picture: Movie5Picture },
-        { id: 6, rating: 4.9, title: "Dark", picture: Movie6Picture },
-    ];
+    const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.target.value;
+    };
+
+    const onClickHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        event.preventDefault();
+    };
+
+    const onSubmitHandler = (event : React.FormEvent<HTMLFormElement>) => {
+        event.target;
+    }
+
+    const onKeyDownHandler = (event : React.KeyboardEvent<HTMLButtonElement>) => {
+        event.key;
+    }
 
     return (
-        <RenderMovies movies={movies}/>
+        <form onSubmit={onSubmitHandler}>
+            <input type="text" onChange={onChangeHandler} />
+            <button onClick={onClickHandler} onKeyDown={onKeyDownHandler}>Click Me</button>
+
+            <CustomInput onChange={(e) => {
+                e.target.value
+            }} value=""/>
+        </form>
     );
 }
 
